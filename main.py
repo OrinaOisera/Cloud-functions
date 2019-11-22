55ff794ad3d4613afc06f49a42c0adb0f7acd7fb
OrinaOisera <orinamark22@gmail.com>
2019-11-22 09:35:03 +0300

    has an xml extractor

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import json
 import random
+from google.cloud import storage
+import xmltodict 
+
+storage_client = storage.Client()
+TICKETS_BUCKET = 'kizee'
 
 def raise_ticket(request):
     content_type = request.headers['content-type'] 
@@ -20,10 +25,30 @@ def raise_ticket(request):
     elif content_type == 'application/x-www-form-urlencoded':
         name = requests.form.get('name')
         issue = requests.form.get('issue')
+    elif content_type == 'text/xml':
+        temp_data = xmltodict.parse(request.data)
+        name = temp_data['ticketraiser']['@name']
+        issue = temp_data['ticketraiser']['issue']
+        
         
     else:
         raise ValueError ("unknown content type {}" . format(content_type))
         
         
     id = random.randint(1000,2000)
-    return'Ticket generated with id, {}'.format(id)
+    
+    tickets_dict={}
+    tickets_dict['id']=id
+    tickets_dict['name']=name
+    tickets_dict['issue']=issue
+    
+    bucket = storage_client.get_bucket(TICKETS_BUCKET)
+    blob = bucket.blob('parsed_tickets_data/tickets.json')
+    if blob.exists():
+        blob.download_to_filename('/tmp/tickets.json')
+    with open("/tmp/tickets.json", "a") as fp:
+        json.dump(tickets_dict,fp,indent=2)
+        
+    blob.upload_from_filename('/tmp/tickets.json')
+    return 'Tickets generated with id, {}'.format(id)
+        
